7eca6c7630121ba54c7ea0277449640aa8a7f96e
thewyolar <thewyolar@gmail.com>
2023-06-02 00:53:23 +0300

    feat: add no access exception

---
+++ service-api/src/main/java/ru/pasteshare/serviceapi/exception/NoAccessException.java
@@ -0,0 +1,7 @@
+package ru.pasteshare.serviceapi.exception;
+
+public class NoAccessException extends Exception {
+    public NoAccessException(String messange) {
+        super(messange);
+    }
+}
