24565a7da95709bcbee898a56d2210e6c72422fd
thewyolar <thewyolar@gmail.com>
2023-06-02 00:52:53 +0300

    feat: add springdoc and modelmapper configurations

---
+++ service-api/src/main/java/ru/pasteshare/serviceapi/config/SpringDocConfig.java
@@ -0,0 +1,29 @@
+package ru.pasteshare.serviceapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI().info(new Info()
+                .title("Paste Share Api")
+                .version("1.0.0"));
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
